c81e0c4311c0d876e9c2d39a57ffd4f0943de942
Marco Antônio da Silva Júnor <marcoasjunior90@gmail.com>
2020-06-29 14:45:33 -0300

    geolib on, descobrindo areas pelo end

--- src/controllers/DemandController.js
+++ src/controllers/DemandController.js
@@ -1,5 +1,7 @@
 const Demand = require("../models/Demand")
 const getGeo = require('../geoCoder')
+const geolib = require('geolib')
+const areas = require('../../areas.json')
 
 module.exports = {
 
@@ -10,10 +12,25 @@ module.exports = {
 
         const geo = await getGeo(address) // Pega coordenadas
 
+        const point = {
+
+            latitude: geo[0].latitude,
+            longitude: geo[0].longitude
+
+        }
+
+        const foundAreas = []
+
+        for (const area of areas) {
+
+            const included = geolib.isPointInPolygon(point, area.polygon.coordinates[0])
+
+            if (included) foundAreas.push(area.name) 
+
+        }
 
 
-
-        res.send(geo)
+        res.send(foundAreas)
 
 
     }
